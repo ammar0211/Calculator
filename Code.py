c221db8142eb991532a43f528b5f2a069255a160
Ammar <32436267+ammar0211@users.noreply.github.com>
2018-11-18 16:31:41 +0530

    Created Button Press Action

--- Code.py
+++ Code.py
@@ -6,3 +6,20 @@ class Calc():
         self.op_pending = False
         self.op = ""
         self.eq = False
+
+        
+
+    def num_press(self, num):
+        self.eq = False
+        temp = text_box.get()
+        temp2 = str(num)      
+        if self.new_num:
+            self.current = temp2
+            self.new_num = False
+        else:
+            if temp2 == '.':
+                if temp2 in temp:
+                    return
+            self.current = temp + temp2
+        self.display(self.current)
+
