fd9a583b3d834b722e26f6b2f49d8817fd75450c
Ammar <32436267+ammar0211@users.noreply.github.com>
2018-11-18 16:30:58 +0530

    Create Calculator Class

--- Code.py
+++ Code.py
@@ -1,1 +1,8 @@
-#
+class Calc():
+    def __init__(self):
+        self.total = 0
+        self.current = ' 5'
+        self.new_num = True
+        self.op_pending = False
+        self.op = ""
+        self.eq = False
